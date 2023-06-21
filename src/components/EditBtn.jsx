cc4bc7093d2ffafee959f9d96c76ced97f246e9f
tomaszbar4 <tomasz.bartnikiewicz1@gmail.com>
2023-06-21 12:21:52 +0200

    first-commit

--- src/components/EditBtn.jsx
+++ src/components/EditBtn.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function EditBtn() {
     return (
         <button className="flex justify-center items-center gap-2 font-bold text-blue-800 md:absolute top-6 right-6 hover:opacity-50 transition-all duration-200">
-            <img src="../../dist//images/icon-edit.svg" />
+            <img src="../../dist/images/icon-edit.svg" />
             <span>Edit</span>
         </button>
     )
