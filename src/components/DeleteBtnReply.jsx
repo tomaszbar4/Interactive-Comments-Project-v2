fad67c912dd05d81554f6af3bed243ef44aace02
tomaszbar4 <tomasz.bartnikiewicz1@gmail.com>
2023-06-21 12:01:15 +0200

    modified

--- src/components/DeleteBtnReply.jsx
+++ src/components/DeleteBtnReply.jsx
@@ -9,7 +9,7 @@ export default function DeleteBtnReply({ toggleReplyModal }) {
     return (
 
         <button onClick={() => toggleReplyModal(true)} className="flex justify-center items-center gap-2 font-bold text-red-500 md:absolute top-6 right-24 hover:opacity-50 transition-all duration-200">
-            <img src="../../public/images/icon-delete.svg" />
+            <img src="../../dist/images/icon-delete.svg" />
             <span>Delete</span>
         </button>
 
--- src/components/ReplyEl.jsx
+++ src/components/ReplyEl.jsx
@@ -25,7 +25,7 @@ export default function ReplyEl({ username, replyActive, reply_id, hideAllReplyE
 
     return (
         <div className="bg-white rounded-md shadow-md p-6 grid grid-cols-2 row-start-2 gap-6 w-11/12 md:justify-evenly md:items-start relative max-w-comment lg:min-w-comment z-0 lg:flex mb-6">
-            <img src="../../public/images/avatars/image-juliusomo.png" className="w-10 row-start-2 lg:row-auto flex items-center" />
+            <img src="../../dist/images/avatars/image-juliusomo.png" className="w-10 row-start-2 lg:row-auto flex items-center" />
             <textarea ref={ref} rows="4" value={msgValue} onChange={(e) => handleChange(e)} className="placeholder:font-semibold border-2 w-full col-span-2 border-solid border-gray-100 rounded-lg p-4 text-sm focus:outline-violet-300 outline-4 resize-none"></textarea>
             <div className="row-start-2 col-start-2 lg:row-auto lg:col-auto flex justify-end items-start" onClick={addReplyAndToggleComment}><RegularBtn>Reply</RegularBtn></div>
         </div >
