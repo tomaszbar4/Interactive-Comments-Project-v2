73cd2303e79762f81d257a680ccf35273efbf822
tomaszbar4 <tomasz.bartnikiewicz1@gmail.com>
2023-06-21 13:43:52 +0200

    first commit

--- src/components/DeleteBtnComment.jsx
+++ src/components/DeleteBtnComment.jsx
@@ -9,7 +9,7 @@ export default function DeleteBtnComment({ toggleCommentModal }) {
     return (
 
         <button onClick={() => toggleCommentModal(true)} className="flex justify-center items-center gap-2 font-bold text-red-500 md:absolute top-6 right-24 hover:opacity-50 transition-all duration-200">
-            <img src="../../dist/images/icon-delete.svg" />
+            <img src="././dist/images/icon-delete.svg" />
             <span>Delete</span>
         </button>
 
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -16,7 +16,7 @@ export default function Reply({ content, user, createdAt, score, id }) {
 
     const ref = useRef(null)
 
-    const imgSrc = `../../dist/` + user.image.png
+    const imgSrc = `././dist/` + user.image.png
 
     const [editMode, setEditMode] = useState(false)
     const [commentText, setCommentText] = useState(content)
@@ -81,9 +81,9 @@ export default function Reply({ content, user, createdAt, score, id }) {
         <>
             <div className="bg-white rounded-md shadow-md p-6 flex w-11/12 gap-6 relative max-w-comment mb-6 z-10" >
                 <div className="bg-gray-100 hidden md:flex flex-col justify-center items-center w-10 max-h-24 rounded-lg gap-3">
-                    <div onClick={upvotePlus}> <img src="../../dist/images/icon-plus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
+                    <div onClick={upvotePlus}> <img src="././dist/images/icon-plus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
                     <span className="text-blue-900 font-bold text-sm ">{upvoteCount}</span>
-                    <div onClick={upvoteMinus}> <img src="../../dist/images/icon-minus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
+                    <div onClick={upvoteMinus}> <img src="././dist/images/icon-minus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
                 </div>
                 <div className="w-full flex flex-col">
                     <div className="flex gap-3 items-center mb-4">
@@ -95,9 +95,9 @@ export default function Reply({ content, user, createdAt, score, id }) {
                     {editMode ? <div className="flex flex-col"><textarea ref={ref} rows="3" value={commentText} onChange={(e) => handleChange(e)} className="border-2 py-3 px-4 focus:outline-0 focus:border-violet-300 border-gray-100 rounded-xl resize-none"></textarea><div className="flex gap-4 mt-4 justify-end"><div onClick={cancel}><CancelBtn /></div><div className="" onClick={handleSubmit}><RegularBtn >Update</RegularBtn></div></div></div> : <p className="text-gray-500 break-words" >{commentText}</p>}
                     <div className="flex justify-between items-center mt-4">
                         <div className="bg-gray-100 flex justify-center md:hidden items-center rounded-lg gap-3 w-24 h-10">
-                            <div onClick={upvotePlus}> <img src="../../dist/images/icon-plus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
+                            <div onClick={upvotePlus}> <img src="././dist/images/icon-plus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
                             <span className="text-blue-900 font-bold text-sm">{upvoteCount}</span>
-                            <div onClick={upvoteMinus}> <img src="../../dist/images/icon-minus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
+                            <div onClick={upvoteMinus}> <img src="././dist/images/icon-minus.svg" className="w-3.5 p-0.5 cursor-pointer" /></div>
                         </div>
                         <div className="flex gap-4">
                             {user.username !== username && <div onClick={openReplyAndHideNewComment}><ReplyBtn /></div>}
